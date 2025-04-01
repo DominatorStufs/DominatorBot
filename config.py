5a2743b0041390c42ae206f612a59a929fc16e6f
DominatorStufs <dpdangergameryt@gmail.com>
2025-04-01 12:43:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 # Your Telegram API credentials
 API_ID = "6301598"  # Replace with your API ID
 API_HASH = "0d273b28f61205ef571461540967255e"  # Replace with your API HASH
-PHONE_NUMBER = "+918920464831"  # Replace with your phone number
+STRING_SESSION = "string_sesion"  # add pyrogramV2 Steing
 
 # Bot prefix configuration
 PREFIX = "."  # You can change this to any character like '.', ',', ';', '?', or leave it empty for no prefix
+
+# Replace with your image URL
+IMAGE_URL = "https://telegra.ph/file/bbb711536dff74138648d.jpg"  # Optional: Set to None if not using an image
+
+# Owner ID
+OWNER_ID = 7837422357
